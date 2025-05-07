09af9a92cc294348351b47f74ce4ef2797f54d50
eospgonz10 <estiven.ospinag@udea.edu.co>
2025-05-06 19:05:09 -0500

    Se crea proyecto spring boot Fabrica Escuela Citas Salud Feature 1 Modulo Login

---
+++ src/main/java/com/udea/EP21F1citasalud_back/Ep21F1citasaludBackApplication.java
@@ -0,0 +1,13 @@
+package com.udea.EP21F1citasalud_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ep21F1citasaludBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ep21F1citasaludBackApplication.class, args);
+	}
+
+}
